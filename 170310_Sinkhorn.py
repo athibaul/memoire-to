f7f7b0c75de9ef1433a8433082b0b1df2cb1dad7
Alexis Thibault <alexis.thibault1@gmail.com>
2017-03-12 15:59:03 +0100

    Initial commit. Add first Sinkhorn and barycenter calculation

---
+++ 170310_Sinkhorn.py
@@ -0,0 +1,107 @@
+import numpy as np
+import scipy.special
+import matplotlib.pyplot as plt
+
+def norme(v):
+    return np.sum(np.abs(v))/len(v)
+
+"""
+Calcule la distance d_M^lbd(r,c), approximation de facteur
+lambda du transport optimal entre r et c.
+r, c : vecteurs de même taille d
+M : matrice d*d des distances
+lambda : quand lambda -> +infini, la valeur approche
+celle du transport optimal non régularisé
+"""
+def ot_sinkhorn(M,r,c,lbd=10,epsilon=1e-3,iter_bound=500):
+    lbd /= len(r)
+    initial_size = (len(r),len(c))
+    I = (r>0)
+    r = r[I]
+    M = M[I,:]
+    K = np.exp(-lbd * M)
+    u = np.ones(len(r))/len(r)
+    norm = 1
+    prev_u = 0
+    count = 0
+    err_r = []; err_c = []
+    #print("I",I,"\nr",r,"\nM",M,"\nK",K,"\nu",u,"\nK_",K_)
+    while norm>epsilon and count<iter_bound:
+        count+=1
+        prev_u = u
+        v = np.divide(c,np.transpose(K).dot(u))
+        u = np.divide(r,K.dot(v))
+        norm = norme(u-prev_u)
+        #err_r.append(norme(u * K.dot(v) - r))
+        #err_c.append(norme(v * np.transpose(K).dot(u) - c))
+        #print(u)
+    if count>=iter_bound:
+        print("Itercount exceeded")
+    #plt.plot(np.log(err_r))
+    #plt.plot(np.log(err_c))
+    #plt.show()
+    #plt.pause(5)
+    #v = np.divide(c,np.transpose(K).dot(u))
+    P = np.zeros(initial_size)
+    P[I,:] = np.diag(u).dot(K).dot(np.diag(v))
+    return P
+    # return np.sum(np.multiply(u,np.multiply(K,M).dot(v)))
+
+def ot_sinkhorn_value(M,*args):
+    P = ot_sinkhorn(M,*args)
+    return np.sum(np.multiply(M,P))
+
+def distances(n,p=2):
+    a = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            a[i,j] = abs(i-j)**p
+    return a
+
+def binomial(n,p=0.5):
+    k = np.arange(n+1)
+    return p**k * (1-p)**(n-k) * scipy.special.comb(n,k)
+
+
+def binomial_distances(n):
+    bin0 = binomial(n)
+    M = distances(len(bin0),0.5)
+    xx = []
+    yy = []
+    for k in range(1,n):
+        print("Computing result ",k,":")
+        p = k/n
+        print("Finding binomial")
+        bin1 = binomial(n,p)
+        print("Applying Sinkhorn")
+        dist = ot_sinkhorn_value(M,bin0,bin1)
+        xx.append(p)
+        yy.append(dist)
+    plt.plot(xx,yy)
+    plt.show()
+
+# binomial_distances(100)
+
+def Gaussian(t,t0,sigma):
+    return np.exp( -(t-t0)**2/(2*sigma**2) )
+
+def normalize(p):
+    return p/sum(p)
+
+def show_transport(n):
+    t = np.arange(n)
+    r0 = normalize(Gaussian(t,n*0.1,n/20)*0.5+Gaussian(t,n*0.5,n/10)+0.02)
+    r1 = normalize(0.5*Gaussian(t,n*0.8,n/20)+Gaussian(t,n*0.5,n/30)+0.02)
+    P = ot_sinkhorn(distances(len(r0),p=2),r0,r1,lbd=1,epsilon=1e-3)
+    ax = plt.subplot2grid((3,3),(1,1),rowspan=2,colspan=2)
+    ax.imshow(np.log(P+1e-5))
+    ax.set_aspect('auto')
+    ax = plt.subplot2grid((3,3),(1,0),rowspan=2)
+    ax.plot(r0,t)
+    ax.invert_xaxis()
+    ax.invert_yaxis()
+    ax = plt.subplot2grid((3,3),(0,1),colspan=2)
+    ax.plot(t,r1)
+    plt.show()
+
+show_transport(1000)
---
+++ 170311_Barycenter.py
@@ -0,0 +1,89 @@
+import numpy as np
+from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
+
+
+def norme(v):
+    return np.sum(np.abs(v))/np.sum(np.ones_like(v))
+
+def Gaussian(t,t0,sigma):
+    return np.exp( -(t-t0)**2/(2*sigma**2) )
+
+def normalize(p):
+    return p/np.sum(p)
+
+"""
+P : array of square images
+lbd : coefficient of each image in the barycenter
+gamma : regularization strength
+"""
+def barycenter(P,lbd=None,gamma=0.04**2):
+    global b,g,g2
+    K = np.size(P,2)
+    N = np.size(P,0)
+    n = N # width of the convolution kernel
+    t = np.linspace(-n/(2*N),n/(2*N),n)
+    g = normalize(np.exp(-t**2 / gamma)); g2 =  np.outer(g,g)
+    def xi(x):
+        return convolve2d(x,g2,'same')
+
+    if lbd is None:
+        lbd = np.ones(K)/K
+    b = np.ones((N,N,K)); a = np.ones((N,N,K))
+    
+    err = []
+    for l in range(100):
+        for k in range(K):
+            a[:,:,k] = np.divide(P[:,:,k],xi(b[:,:,k]))
+            
+        log_q = np.zeros((N,N))
+        for k in range(K):
+            log_q += lbd[k] * np.log(np.maximum(1e-19, b[:,:,k] * xi(a[:,:,k])))
+        q = np.exp(log_q)
+        
+        for k in range(K):
+            b[:,:,k] = np.divide(q,xi(a[:,:,k]))
+            
+        err.append(np.array([norme(a[:,:,k] * xi(b[:,:,k]) - P[:,:,k]) for k in range(K)]))
+    
+    return q
+
+
+def test0():
+    n_2 = 20
+    r = 16
+    N = 2*n_2
+    P = np.array([[x*x+y*y < r*r for x in range(-n_2,n_2)] for y in range(-n_2,n_2)])
+    plt.subplot(121)
+    plt.imshow(P)
+    plt.colorbar()
+    
+    n = 41
+    gamma = 0.04**2
+    t = np.linspace(-n/(2*N),n/(2*N),n)
+    g = normalize(np.exp(-t**2 / gamma)); g2 = np.outer(g,g)
+    def xi(x):
+        return convolve2d(x,g2,'same')
+    
+    plt.subplot(122)
+    plt.imshow(xi(P))
+    plt.colorbar()
+    plt.show()
+
+def test1():
+    global q
+    n = 50
+    t = np.arange(n)
+    rep1 = normalize(np.array([[(x>3)*(x<10) == (y>n/2) for x in range(n)] for y in range(n)]))
+    rep2 = np.outer(Gaussian(t,0.1*n,0.05*n),Gaussian(t,0.5*n,0.2*n))
+    rep2 = normalize(rep2)
+    P = np.zeros((n,n,2))
+    P[:,:,0] = rep1; P[:,:,1] = rep2
+    
+    number_of_subplots = 5
+    coefs = np.linspace(0,1,number_of_subplots)
+    for i,alpha in enumerate(coefs):
+        lbd = np.array([alpha, 1-alpha])
+        ax1 = plt.subplot(1,number_of_subplots,i+1)
+        ax1.imshow(barycenter(P,lbd))
+    plt.show()
